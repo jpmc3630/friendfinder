80c5bffc6ad42751f0ec2f395cbe4ebbc75d8f5b
James McGlone <jamespmcglone@gmail.com>
2020-01-04 16:42:05 +0800

    html routes working

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+console.log('API Route Connected');
+
+
+// Link in Friends Data
+// var friendsData = require('../data/friends.js');
+
+
+// Includes Two Routes
+function apiRoutes(app) {
+
+
+}
+
+
+// Export for use in main server.js file
+module.exports = apiRoutes;
---
+++ server.js
@@ -0,0 +1,31 @@
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var apiReq = require("./app/routing/apiRoutes.js");
+var htmlReq = require("./app/routing/htmlRoutes.js");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// API and HTML routes
+apiReq(app);
+htmlReq(app);
+
+
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
+
